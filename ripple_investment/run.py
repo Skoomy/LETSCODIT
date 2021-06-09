7f441f17c824a2311c9ea834a379bf2fd74330fe
Tbx <2510205-Tb.x@users.noreply.gitlab.com>
2021-06-09 05:50:04 +0200

    Add code

--- ripple_investment/run.py
+++ ripple_investment/run.py
@@ -1,85 +1,87 @@
-import pandas as pd 
-# from sklearn
+import pandas as pd
+import time
 import click
-from ripple import feature
+from ripple import feature, utils, strategy, logger
+import logging
+import numpy as np
+
+# logger = logging.getLogger(__name__)
 
 
+def meta_setting(data, name="sample"):
+    """ """
+    if "time" not in data.columns:
+        raise ValueError("date not in columns")
 
-def meta_setting(data,name='sample'):
-    '''
-    '''
-    if 'time' not in data.columns:
-        raise ValueError('date not in columns')
-        
     result = {}
-    result['start_period'] = data['time'].min().__str__()
-    result['end_period'] = data['time'].max().__str__()
-    result['nb_obs'] = data.shape[0]
-    result['name'] = name
-    
+    result["start_period"] = data["time"].min().__str__()
+    result["end_period"] = data["time"].max().__str__()
+    result["nb_obs"] = data.shape[0]
+    result["name"] = name
+
     return result
 
 
-
-def train_test_split(data,ts_size=0.7):
-    '''
-    '''
-    size = len(data) 
-    train_size = int(size * ts_size )
-    xtrain = data.iloc[0:train_size]
-   
-       
-    x_val = data.iloc[train_size:size]
-    size_val = len(x_val)
-    size_xtest = int(0.3 *size_val)
-    xtest = x_val.iloc[0:size_xtest]
-    xval = x_val.iloc[size_xtest:size_val]
-    
-
-    return xtest, xval, xtrain
-    
-    
-
-    
-    
 def load():
-    data = pd.read_csv('data/XRP_price_20210524.csv')
+    data = pd.read_csv("data/XRP_price_20210608.csv")
+    data["date"] = data["time"].apply(utils.to_date)
     return data
-
 
 
 def run():
     data = load()
     data = feature.transform(data).dropna()
-    
-    df_down,result = feature._trend(data,'decreasing')
-    df_down.to_csv('data/down_data.csv',index=False)
-    result.to_csv('data/down_analysis.csv',index=False)
-    
-    #=======
-    df_up,result = feature._trend(data,'increasing')
-    result.to_csv('data/up_analysis.csv',index=False)
-    df_up.to_csv('data/up_data.csv',index=False)
-    
-    
+
+    df_down, result = feature._trend(data, "decreasing")
+    df_down.to_csv("data/down_data.csv", index=False)
+    result.to_csv("data/down_analysis.csv", index=False)
+
+    # =======
+    df_up, result = feature._trend(data, "increasing")
+    result.to_csv("data/up_analysis.csv", index=False)
+    df_up.to_csv("data/up_data.csv", index=False)
+
     settings = []
-    xtest,xval,xtrain = train_test_split(data)
+    xtest, xval, xtrain = utils.train_test_split(data)
     print(xtest)
-    
-    for (k,n) in zip([xtest,xval,xtrain],['test','val','train']):
-       
-        settings.append(meta_setting(data=k,name=n))
-    
-#     xtest = feature._trend(xtest)
+
+    for (k, n) in zip([xtest, xval, xtrain], ["test", "val", "train"]):
+
+        settings.append(meta_setting(data=k, name=n))
+
+    #     xtest = feature._trend(xtest)
     settings = pd.DataFrame(settings)
-    settings.to_csv('data/settings.csv',index=False)
+    settings.to_csv("data/settings.csv", index=False)
     print(settings)
-    
+
     return data
 
 
-if __name__ == '__main__':
-    click.secho('Run ripple',fg='green')
-   
-    run()
-    
+def run_strat(data, _cash=10, period=3, name="ripple_trend"):
+
+    click.clear()
+
+    click.secho("\t\t\t .....Starting computation ......\n", fg="green")
+    logger.info("{} execution".format(run))
+    tic = time.time()
+
+    actif = strategy.runner(data=data, cash=_cash, name=name, period=period)
+
+    pnl = actif.broker.getvalue() - _cash
+    returns = np.round((actif.broker.getvalue() - _cash) / _cash, 4) * 100
+    msg = "\n\t\tPNL {0}\n \t\tReturns : {1} % ".format(pnl, returns)
+    click.secho(msg, fg="red")
+    print("-" * 100)
+    computation_time = time.time() - tic
+    msg = "took {} ".format(computation_time)
+    click.secho(msg, fg="green")
+    return
+
+
+if __name__ == "__main__":
+    click.secho("Run Ripple", fg="green")
+    data = load()
+    data = data[data["date"] > "2021-03-01"]
+    # data = data[data["date"] < "2021-07-01"]
+    run_strat(data, _cash=100)
+    # run()
